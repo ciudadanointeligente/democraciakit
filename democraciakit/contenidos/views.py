2efcd8cb916de8a278a42785621859d691715204
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2024-08-20 02:35:10 -0400

    update #2 primera parte

--- democraciakit/contenidos/views.py
+++ democraciakit/contenidos/views.py
@@ -19,3 +19,32 @@ class Etapa1ConceptosView(TemplateView):
 class Etapa1DefinicionView(TemplateView):
     template_name = 'contenidos/etapa1-definicion.html'
     
+class Etapa2View(TemplateView):
+    template_name = 'contenidos/etapa2.html'
+
+class Etapa2MatrizView(TemplateView):
+    template_name = 'contenidos/etapa2-matriz.html'
+
+class Etapa2MapaAfinidadView(TemplateView):
+    template_name = 'contenidos/etapa2-mapaafinidad.html'
+
+class Etapa2InclusivoView(TemplateView):
+    template_name = 'contenidos/etapa2-inclusivo.html'
+
+class Etapa2IdentificacionView(TemplateView):
+    template_name = 'contenidos/etapa2-identificacion.html'
+
+class Etapa3View(TemplateView):
+    template_name = 'contenidos/etapa3.html'
+
+class Etapa4View(TemplateView):
+    template_name = 'contenidos/etapa4.html'
+
+class Etapa5View(TemplateView):
+    template_name = 'contenidos/etapa5.html'
+
+class Etapa6View(TemplateView):
+    template_name = 'contenidos/etapa6.html'
+
+class Etapa7View(TemplateView):
+    template_name = 'contenidos/etapa7.html'
