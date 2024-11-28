bd446142d0ae0627cd2671138a130c24b6113502
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2024-11-28 16:42:41 -0300

    admin users

--- democraciakit/users/admin.py
+++ democraciakit/users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser, Pais
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "pais", "municipio")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Pais)
